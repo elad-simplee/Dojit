d246f83ecf393a84ea79cbf96da315d42ace962b
Elad Cohen <elad.c@simplee.com>
2015-08-19 14:34:03 +0300

    add validations to posts creating and editing. render some partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-  def my_name
-    "Elad Cohen"
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
   end
 end
