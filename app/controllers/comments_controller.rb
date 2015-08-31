65d09f30b14785977d2c05d26f7dc0055a8a5ec0
Elad Cohen <elad.c@simplee.com>
2015-08-31 16:06:48 +0300

    destroy comments using ajax referencing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def create
     @post = Post.find(params[:post_id])
     @topic = @post.topic
@@ -23,11 +25,14 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
     end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
+
   end
 
   def post_url (post)
